657c7d20e003647ba3fcf3fbd50b200fc5aafa75
Wangyue0124 <diego960124@gmail.com>
2025-09-28 17:47:08 +0800

    Add files via upload

---
+++ c007. 00272 - TeX Quotes.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    string s;
+    bool TeX = false;
+    while (getline(cin,s)) {
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == '"') {
+                if (TeX) {
+                    cout << "''";
+                    TeX = false;
+                }
+                else {
+                    cout << "``";
+                    TeX = true;
+                }
+            }
+            else cout << s[i];
+        }
+        cout << "\n";
+    }
+    return 0;
+}
